a7d8d6a2251b58a9459bec28bdc96173b94e9d2f
Alejandro Perez <alejandro.perez.mendez@gmail.com>
2017-05-25 14:28:56 +0200

    Federation code now has its dedicated sublcass of AuthorizationServiceConfiguration

--- app/java/net/openid/appauthdemo/LoginActivity.java
+++ app/java/net/openid/appauthdemo/LoginActivity.java
@@ -42,6 +42,7 @@ import net.openid.appauth.AuthorizationRequest;
 import net.openid.appauth.AuthorizationService;
 import net.openid.appauth.AuthorizationServiceConfiguration;
 import net.openid.appauth.ClientSecretBasic;
+import net.openid.appauth.FederatedAuthorizationServiceConfiguration;
 import net.openid.appauth.RegistrationRequest;
 import net.openid.appauth.RegistrationResponse;
 import net.openid.appauth.ResponseTypeValues;
@@ -197,7 +198,7 @@ public final class LoginActivity extends AppCompatActivity {
         // WrongThread inference is incorrect for lambdas
         // noinspection WrongThread
         runOnUiThread(() -> displayLoading("Retrieving discovery document"));
-        AuthorizationServiceConfiguration.fetchFromUrl(
+        FederatedAuthorizationServiceConfiguration.fetchFromUrl(
                 mConfiguration.getDiscoveryUri(),
                 this::handleConfigurationRetrievalResult,
                 mConfiguration.getConnectionBuilder(),
